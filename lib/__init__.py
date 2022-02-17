aecaa6a7dbf565ce47f1b7a760ed6618c486a352
Lucchesi <rodrigo@devgrid.co.uk>
2022-02-16 23:21:40 -0300

    Adding steps to project and lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -20,6 +20,28 @@ import seaborn as sns
 # TODO: 
 
 - Review Project Code and map the instance methods with the project requirements 
+
+Instance methods to be created to this lib:
+
+1. read_csv
+2. statistics
+3. encode to binary categorical
+4. histogram target, column
+5. bar_plot df col normalized
+6. dist_plot col
+7. heatmap sns correlation matrix
+8. define hypothesy matrix and target matrix
+9. encode columns with groupby ... understand groupby
+10. train_test_split
+11. model tunning with GridSearch -> get params and model
+12  plot_roc_curve ?
+13. save best model with joblib
+14. open and test model
+15. shap.TreeExplainer(cv_rfc.best_estimator_)
+16. feature importance with bar plot  
+17. Report model tunning
+
+
 - Write the code with the test and document function by function
 - Each method
   - pep8 and pylint -> Friday first working version
